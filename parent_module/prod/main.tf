444f8483849a941d71e169a720938720da38af1f
shripalsinghdevops <shripal.singhdevops@gmail.com>
2026-08-12 10:04:55 +0530

    new resource added

---
+++ parent_module/prod/main.tf
@@ -0,0 +1,52 @@
+module "resource_group" {
+    source = "../../child_modules/azurerm_resource_group"
+rgs = {
+
+rg1 = {
+
+    name = "shri"
+    location = "centralindia"
+}
+ 
+}
+}
+
+module "virtual_network" {
+    source = "../../child_modules/azurerm_virtual_network"
+depends_on = [ module.resource_group ]
+    vnet = {
+  vnet1 = {
+    vnet_name           = "vnet1"
+    location            = "westus"
+    resource_group_name = "shri"
+    address_space       = ["10.0.0.0/16"]
+
+  }
+  
+}
+}
+module "subnetwork" {
+    source = "../../child_modules/azurerm_subnetwork"
+    depends_on = [module.virtual_network]
+subnet ={
+subnet1 ={
+subnet_name                 = "frontend"
+resource_group_name  = "shri"
+virtual_network_name = "vnet1"
+address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+
+subnet2 ={
+subnet_name                 = "backend"
+resource_group_name  = "shri"
+virtual_network_name = "vnet1"
+address_prefixes     = ["10.0.2.0/24"]
+
+    }
+}
+
+  
+}
+
