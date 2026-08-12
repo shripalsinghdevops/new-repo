703e2b3acb99e8e07474c2e4bb62813faaac16b4
shripalsinghdevops <shripal.singhdevops@gmail.com>
2026-08-12 10:46:21 +0530

    updated yaml file

--- parent_module/prod/provider.tf
+++ parent_module/prod/provider.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "shri"
-    storage_account_name = "abcd123454321"
-    container_name       = "container"
-    key                  = "pipeline.tfstate"
+  # backend "azurerm" {
+  #   resource_group_name  = "shri"
+  #   storage_account_name = "abcd123454321"
+  #   container_name       = "container"
+  #   key                  = "pipeline.tfstate"
 
-  }
+  # }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
